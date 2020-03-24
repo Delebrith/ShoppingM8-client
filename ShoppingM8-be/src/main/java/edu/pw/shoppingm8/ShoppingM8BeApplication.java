107b91649b1a18ed040833a570405548c048f454
Delebrith <paulina.szwed.1996@gmail.com>
2020-03-24 14:43:30 +0100

    BE stub

---
+++ ShoppingM8-be/src/main/java/edu/pw/shoppingm8/ShoppingM8BeApplication.java
@@ -0,0 +1,13 @@
+package edu.pw.shoppingm8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingM8BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingM8BeApplication.class, args);
+	}
+
+}
