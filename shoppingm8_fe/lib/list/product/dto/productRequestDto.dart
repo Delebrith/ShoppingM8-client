880e71688a726ce9a13fdf76b5382bd8c3cde77a
Delebrith <paulina.szwed.1996@gmail.com>
2020-04-27 00:51:42 +0200

    draft implementation of product list view and product creation dialog

--- shoppingm8_fe/lib/list/product/dto/productRequestDto.dart
+++ shoppingm8_fe/lib/list/product/dto/productRequestDto.dart
@@ -3,9 +3,8 @@ import '../productCategory.dart';
 class ProductRequestDto {
   final String name;
   final num requiredAmount;
-  final num purchasedAmount;
   final String unit;
   final ProductCategory category;
 
-  ProductRequestDto({this.name, this.requiredAmount, this.purchasedAmount, this.unit, this.category});
+  ProductRequestDto({this.name, this.requiredAmount, this.unit, this.category});
 }
--- shoppingm8_fe/lib/list/product/productCreationDialog.dart
+++ shoppingm8_fe/lib/list/product/productCreationDialog.dart
@@ -3,20 +3,38 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shoppingm8_fe/common/dto/errorDto.dart';
+import 'package:shoppingm8_fe/list/product/productCategory.dart';
+import 'package:dropdown_formfield/dropdown_formfield.dart';
 
 import 'dto/productRequestDto.dart';
 import 'productApiProvider.dart';
 
-class ProductCreationDialog extends StatelessWidget {
-  final GlobalKey<FormState> _listForm = GlobalKey<FormState>();
+class ProductCreationDialog extends StatefulWidget {
+  final String title;
+  final ProductApiProvider apiProvider;
+  final Function onSuccess;
+
+  ProductCreationDialog({Key key, this.title, this.apiProvider, this.onSuccess}) : super(key: key);
+
+  @override
+  _ProductCreationWidgetState createState() => _ProductCreationWidgetState(title: this.title, apiProvider: this.apiProvider, onSuccess: this.onSuccess);
+
+}
+
+class _ProductCreationWidgetState extends State {
+  final GlobalKey<FormState> _productForm = GlobalKey<FormState>();
   final String title;
   final ProductApiProvider apiProvider;
   final Function onSuccess;
 
   String _name;
+  num _requiredAmount;
+  String _unit;
+  ProductCategory _category;
 
-  ProductCreationDialog({Key key, this.title, this.apiProvider, this.onSuccess}) : super(key: key);
+  _ProductCreationWidgetState({this.title, this.apiProvider, this.onSuccess});
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +43,12 @@ class ProductCreationDialog extends StatelessWidget {
       content: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Form(
-              key: _listForm,
+              key: _productForm,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   Padding(
@@ -36,8 +56,42 @@ class ProductCreationDialog extends StatelessWidget {
                     child: TextFormField(
                       autofocus: true,
                       autocorrect: false,
-                      decoration: InputDecoration(labelText: "list name"),
+                      decoration: InputDecoration(labelText: "Product name"),
                       onSaved: (value) => _name = value,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      autocorrect: false,
+                      keyboardType: TextInputType.numberWithOptions(decimal: true),
+                      inputFormatters: <TextInputFormatter>[
+                        WhitelistingTextInputFormatter.digitsOnly
+                      ],
+                      decoration: InputDecoration(labelText: "Required amount"),
+                      onSaved: (value) => _requiredAmount = value as num,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      autocorrect: false,
+                      decoration: InputDecoration(labelText: "Unit (kg, g, pc...)"),
+                      onSaved: (value) => _unit = value,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: DropDownFormField(
+                      dataSource: _getDropdownItems(),
+                      titleText: "Product category",
+                      textField: "display",
+                      valueField: "value",
+                      onSaved: (value) => _category = value,
+                      onChanged: (value) => setState(() {
+                        _category = value;
+                      }),
+                      value: _category,
                     ),
                   ),
                   Padding(
@@ -63,8 +117,8 @@ class ProductCreationDialog extends StatelessWidget {
   }
 
   Future<void> _submit(context) async {
-    _listForm.currentState.save();
-    ProductRequestDto dto = ProductRequestDto(name: _name);
+    _productForm.currentState.save();
+    ProductRequestDto dto = ProductRequestDto(name: _name, category: _category, requiredAmount: _requiredAmount, unit: _unit);
     Response response = await apiProvider.createProduct(dto);
     if (response.statusCode == 201) {
       Navigator.pop(context);
@@ -87,4 +141,17 @@ class ProductCreationDialog extends StatelessWidget {
       );
     }
   }
+
+  List<Map<String, Object>> _getDropdownItems() {
+    return ProductCategory.values
+        .map((val) => {"display": ProductCategoryHepler.getName(val), "value": val})
+        .toList();
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
 }
